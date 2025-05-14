085a57a2bac4798f4f2982d903db8c84f85a08ff
Leonard <leonardglez12485@gmail.com>
2025-05-14 10:16:34 -0300

    Fix formatting and update assembly informational version

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,5 +9,5 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
-        public DbSet<Product> Products { get; set; }
-    }
+    public DbSet<Product> Products { get; set; }
+}
